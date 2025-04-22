32eda69bd406695c4ef4d470073ab6d72f8d00af
COT-WORLD <hardikchaudhary.aus@gmail.com>
2025-04-22 16:21:31 +1000

    deployment ready for render

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from database import create_db_and_tables
 from contextlib import asynccontextmanager
 from routers import post, user, vote
 import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -11,6 +12,14 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(post.router)
